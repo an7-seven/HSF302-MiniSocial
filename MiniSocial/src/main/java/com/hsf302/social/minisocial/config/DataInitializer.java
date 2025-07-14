9e39fb546e26de921b82a417d9f35205d2404e60
an7-seven <vietann.0409@gmail.com>
2025-07-14 16:25:56 +0700

    Update DataInitializer.java

--- MiniSocial/src/main/java/com/hsf302/social/minisocial/config/DataInitializer.java
+++ MiniSocial/src/main/java/com/hsf302/social/minisocial/config/DataInitializer.java
@@ -15,16 +15,33 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        for (int i = 1; i <= 10; i++) {
+        String[] usernames = {"johnny", "alice_dev", "mikejava", "sandy.tech", "tonyreact", "linhcute", "ducnode", "hana_hihi", "boss_bao", "testguy999"};
+        String[] emails = {"johnny@demo.com", "alice@demo.com", "mike@demo.com", "sandy@demo.com", "tony@demo.com", "linh@demo.com", "duc@demo.com", "hana@demo.com", "bao@demo.com", "testguy@demo.com"};
+        String[] fullNames = {"Johnny Walker", "Alice Nguyen", "Mike Le", "Sandy Tran", "Tony Nguyen", "Linh Vu", "Duc Le", "Hana Pham", "Bao Ho", "Tester Anonymous"};
+        String[] bios = {
+                "Loves traveling and photography.",
+                "Frontend dev & coffee addict.",
+                "Java enthusiast.",
+                "I test stuff for a living.",
+                "React fanboy & anime watcher.",
+                "Just here for the memes.",
+                "Node.js + late-night coding.",
+                "Lofi + study = mood.",
+                "Backend is life.",
+                "Don’t mind me. I break things."
+        };
+
+        for (int i = 0; i < 10; i++) {
             User user = new User();
-            user.setUserName("user" + i);
-            user.setPassword("password" + i);
-            user.setEmail("user" + i + "@example.com");
-            user.setFullName("User Number " + i);
-            user.setAvatarUrl("https://example.com/avatar" + i + ".png");
-            user.setBio("This is bio of user " + i);
+            user.setUserName(usernames[i]);
+            user.setPassword("pass123");
+            user.setEmail(emails[i]);
+            user.setFullName(fullNames[i]);
+            user.setAvatarUrl("https://i.pravatar.cc/150?img=" + (i + 1));
+            user.setBio(bios[i]);
             userService.save(user);
         }
+
 
     }
 }
